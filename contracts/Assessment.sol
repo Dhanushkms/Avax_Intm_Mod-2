1fe26ffa63a1b5490fa12623dd33d02f57133233
Dhanushkms <143531988+Dhanushkms@users.noreply.github.com>
2023-10-13 23:19:56 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract SimpleAssessment {
+contract Assessment {
     address public owner;
-    uint256 public balance;
+    uint256 public blc;
 
     event Deposited(uint256 amount);
     event Withdrawn(uint256 amount);
@@ -11,23 +11,23 @@ contract SimpleAssessment {
 
     constructor(uint256 initialBalance) {
         owner = msg.sender;
-        balance = initialBalance;
+        blc = initialBalance;
     }
 
-    function checkBalance() public view returns (uint256) {
-        return balance;
+    function balance() public view returns (uint256) {
+        return blc;
     }
 
     function deposit(uint256 amount) public payable {
         require(msg.sender == owner, "Not authorized to deposit");
-        balance += amount;
+        blc += amount;
         emit Deposited(amount);
     }
 
     function withdraw(uint256 amount) public {
         require(msg.sender == owner, "Not authorized to withdraw");
-        require(balance >= amount, "Insufficient balance");
-        balance -= amount;
+        require(blc >= amount, "Insufficient balance");
+        blc -= amount;
         emit Withdrawn(amount);
     }
 
