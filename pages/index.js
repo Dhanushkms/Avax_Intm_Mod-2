fd81adb6944207a3279084b00a1ae7e2f5cd46b0
Dhanushkms <143531988+Dhanushkms@users.noreply.github.com>
2023-10-13 19:13:20 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,117 +1,242 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
-import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
+const contractAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; 
+
+const atm_abi = [ // Replace with the updated ABI
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "initialBalance",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "Deposited",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "Withdrawn",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "balance",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
 
 export default function HomePage() {
-  const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
-  const [atm, setATM] = useState(undefined);
-  const [balance, setBalance] = useState(undefined);
+  const [balance, setBalance] = useState(0);
+  const [inputA, setInputA] = useState(0);
+  const [inputB, setInputB] = useState(0);
+  const [result, setResult] = useState(0);
+  const [currentTime, setCurrentTime] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
 
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const atmABI = atm_abi.abi;
-
-  const getWallet = async() => {
+  const checkWallet = async () => {
     if (window.ethereum) {
-      setEthWallet(window.ethereum);
+      const accounts = await window.ethereum.request({ method: "eth_accounts" });
+      if (accounts.length > 0) {
+        setAccount(accounts[0]);
+      }
     }
-
-    if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
-      handleAccount(account);
-    }
-  }
-
-  const handleAccount = (account) => {
-    if (account) {
-      console.log ("Account connected: ", account);
-      setAccount(account);
-    }
-    else {
-      console.log("No account found");
-    }
-  }
-
-  const connectAccount = async() => {
-    if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
-      return;
-    }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
-    handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
-    getATMContract();
   };
 
-  const getATMContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethWallet);
+  const getBalance = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const contract = new ethers.Contract(contractAddress, atm_abi, provider);
+    const currentBalance = await contract.balance();
+    setBalance(currentBalance.toNumber());
+  };
+
+  const deposit = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
+    const contract = new ethers.Contract(contractAddress, atm_abi, signer);
+    const transaction = await contract.deposit(inputA, { value: inputA });
+    await transaction.wait();
+    getBalance();
+  };
 
-  const getBalance = async() => {
-    if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+  const withdraw = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, atm_abi, signer);
+    const transaction = await contract.withdraw(inputB);
+    await transaction.wait();
+    getBalance();
+  };
+
+  const calculateResult = (operator) => {
+    if (operator === "+") {
+      setResult(parseFloat(inputA) + parseFloat(inputB));
+    } else if (operator === "-") {
+      setResult(parseFloat(inputA) - parseFloat(inputB));
     }
-  }
+  };
 
-  const deposit = async() => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
-    }
-  }
-
-  const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
-      getBalance();
-    }
-  }
-
-  const initUser = () => {
-    // Check to see if user has Metamask
-    if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
-    }
-
-    // Check to see if user is connected. If not, connect to their account
-    if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
-    }
-
-    if (balance == undefined) {
-      getBalance();
-    }
-
-    return (
-      <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
-      </div>
-    )
-  }
-
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => {
+    checkWallet();
+    getBalance();
+    const interval = setInterval(() => {
+      const date = new Date();
+      setCurrentTime(date.toLocaleTimeString());
+      setCurrentDate(date.toLocaleDateString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
+      <header>
+        <h1>Welcome!</h1>
+        <h2>Ethereum Wallet</h2>
+      </header>
+      {account ? (
+        <div>
+          <p>Your Account: {account}</p>
+          <p>Your Balance: {balance} ETH </p>
+
+          <input
+            type="number"
+            placeholder="Enter ETH to deposit"
+            value={inputA}
+            onChange={(e) => setInputA(e.target.value)}
+          />
+          <button style={{ backgroundColor: "green" }} onClick={deposit}>
+            Deposit ETH
+          </button>
+          <br />
+
+          <input
+            type="number"
+            placeholder="Enter ETH to withdraw"
+            value={inputB}
+            onChange={(e) => setInputB(e.target.value)}
+          />
+          <button style={{ backgroundColor: "#FF0000" }} onClick={withdraw}>
+            Withdraw ETH
+          </button>
+        </div>
+      ) : (
+        <p>Please install Metamask to use this ATM.</p>
+      )}
+
+      <div>
+        <h2>Crypto Calculator</h2>
+        <p>Result: {result}</p>
+
+        <input
+          type="number"
+          placeholder="Enter ETH"
+          value={inputA}
+          onChange={(e) => setInputA(e.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Enter ETH"
+          value={inputB}
+          onChange={(e) => setInputB(e.target.value)}
+        />
+        <br />
+
+        <p>Perform Operation:</p>
+
+        <button style={{ backgroundColor: "grey" }} onClick={() => calculateResult("+")}>
+          +
+        </button>
+        <button style={{ backgroundColor: "grey" }} onClick={() => calculateResult("-")}>
+          -
+        </button>
+      </div>
+      <div>
+        <p>Current Date: {currentDate}</p>
+        <p>Current Time: {currentTime}</p>
+      </div>
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
         }
-      `}
-      </style>
+      `}</style>
     </main>
-  )
+  );
 }
+
