ae0c0c39bbffa907900a056233654cf21e1c2ece
Dhanushkms <143531988+Dhanushkms@users.noreply.github.com>
2023-10-13 18:27:39 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
-contract Assessment {
-    address payable public owner;
+contract SimpleAssessment {
+    address public owner;
     uint256 public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposited(uint256 amount);
+    event Withdrawn(uint256 amount);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    constructor(uint256 initialBalance) {
+        owner = msg.sender;
+        balance = initialBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function checkBalance() public view returns (uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function deposit(uint256 amount) public payable {
+        require(msg.sender == owner, "Not authorized to deposit");
+        balance += amount;
+        emit Deposited(amount);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    function withdraw(uint256 amount) public {
+        require(msg.sender == owner, "Not authorized to withdraw");
+        require(balance >= amount, "Insufficient balance");
+        balance -= amount;
+        emit Withdrawn(amount);
+    }
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
+    function transferOwnership(address newOwner) public {
+        require(msg.sender == owner, "Not authorized to transfer ownership");
+        require(newOwner != address(0), "Invalid address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+    function getOwner() public view returns (address) {
+        return owner;
     }
 }
